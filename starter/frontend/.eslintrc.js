24e662a9e312fb672725fd5a42569d8172239389
Lam Ngo <115174499+nimblelancer@users.noreply.github.com>
2024-12-15 22:28:45 +0700

    Update .eslintrc.js

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,38 +1,44 @@
 module.exports = {
-    ignorePatterns: ['build/', 'dist/', 'node_modules/'],
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    plugins: ['react', 'prettier'],
-    settings: {
-      react: {
-        version: 'detect',
+  ignorePatterns: ['build/', 'dist/', 'node_modules/'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Ensure Prettier integration
+  ],
+  plugins: ['react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: false,
+        printWidth: 120,
+        trailingComma: 'es5', // Adds consistent trailing commas
+        bracketSpacing: true, // Adds spacing in object literals
+        arrowParens: 'always', // Adds parentheses around arrow function arguments
+        tabWidth: 2, // Consistent indentation
+        useTabs: false, // Spaces over tabs
       },
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true,
-    },
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          jsxSingleQuote: false,
-          printWidth: 120,
-          parser: 'babel-ts',
-        },
-      ],
-      // Customize your rules here
-      'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
-      'react/jsx-uses-vars': 'warn',
-    },
-  };
-  
+    ],
+    'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
+    'react/jsx-uses-vars': 'warn',
+  },
+};
